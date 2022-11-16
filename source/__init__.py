01c324ad02dbb80493511f301108028f7855a2d3
Blandine <blandine.willemart@student.unamur.be>
2022-11-16 11:58:36 +0100

    code à débuguer
    création de compte

--- source/__init__.py
+++ source/__init__.py
@@ -1,3 +1,33 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
+from config import Config
+
+def initDB():
+    """
+    Initialisation of the database.
+    """
+    db.create_all()
+
+    db.session.commit()
+
 app = Flask(__name__)
+app.config.from_object(Config)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "warning"
+login_manager.login_message = "You cannot acces to this page. Please log in."
+login_manager.session_protection = 'strong'
+
+# Database part :
+db = SQLAlchemy(app)
+
+# Socket parts :
+socketio = SocketIO(app)
+
 from source import code
+from source import models
+
+initDB()
---
+++ source/models.py
@@ -0,0 +1,69 @@
+from flask_login import UserMixin
+from source import db
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+from source import login_manager
+
+
+class Account(UserMixin, db.Model) :
+    """
+    Account class
+    --------------
+    This class will contain all the information about the users who have an account.
+    """
+    #Initialisation
+    __tablename__ = 'account'
+
+    #DB
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    username = db.Column(db.String(255), nullable = False, unique = True)
+    lastName = db.Column(db.String(255), nullable = False)
+    firstName = db.Column(db.String(255), nullable = False)
+    email = db.Column(db.String(255), nullable = False, unique = True)
+    password = db.Column(db.String(255), nullable = False)
+
+    def getUsername(self) :
+        """
+        Getter of the username.
+        Return : 
+        ----------
+        Username
+        """
+        return self.id
+
+    def setPassword(self, password) :
+        """
+        Setter of the password in the database.
+        We use for that the function generate_password_hash so that the password is cripted in the database.
+        """
+        self.password = generate_password_hash(password)
+        db.session.commit()
+    
+    def checkPassword(self, password) :
+        """
+        Check if the password do correspond.
+        Return :
+        ----------
+        True if the passwords do correspond, False otherwise.
+        """
+        return check_password_hash(self.password, password)
+
+    def __repr__(self) :
+        """
+        How users will be represented.
+        Return :
+        ----------
+        Representation of a user.
+        """
+        return "<User id : %d, username : %s, lastName : %s, firstName : %s, email : %s>" % (self.id, self.username, self.lastName, self.firstName, self.email)
+
+#The DB part :
+db.drop_all()
+db.create_all()
+
+
+#Hardcode of my user in the DB (just for convenience)
+utilisateurs = Account(username = "willemab", lastName = "Willemart", firstName = "Blandine", email = "blandine.willemart@student.unamur.be", password = "mypassword"
+db.session.add(utilisateurs)
+
+db.session.commit()
