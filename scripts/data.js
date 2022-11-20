9762883c3f397ad5943c1cb9204392fe3e729a67
SimLej18 <simonlejoly@icloud.com>
2022-11-20 16:22:42 +0100

    get methods now return objects

--- scripts/data.js
+++ scripts/data.js
@@ -17,8 +17,12 @@ class Stage {
     /*
     * This class defines a stage (aka. a stop, a halt) in an excursion
     */
-    constructor() {
-
+    constructor(name, longitude, latitude) {
+        if (name === "null" || longitude === "null" || latitude === "null")
+            throw new Error("Mandatory field is null.")
+        this.name = name;
+        this.longitude = longitude
+        this.latitude = latitude
     }
 
 }
@@ -27,8 +31,14 @@ class Event extends Stage {
     /*
     * This class defines an event, stages only available for a limited time
     */
-    constructor() {
-        super();
+    constructor(name, longitude, latitude, start, finish, address, description, link) {
+        super(name, longitude, latitude);
+        this.name = name;
+        this.start = start;
+        this.finish = finish;
+        this.address = address;
+        this.description = description;
+        this.link = link;
     }
 }
 
@@ -36,9 +46,12 @@ class Museum extends Stage {
     /*
     * This class defines a museum, building where you can make a visit
     */
-    constructor() {
-        super();
-
+    constructor(name, longitude, latitude, address, description, phone, mail) {
+        super(name, longitude, latitude);
+        this.address = address;
+        this.description = description;
+        this.phone = phone;
+        this.mail = mail;
     }
 }
 
@@ -46,141 +59,100 @@ class Monument extends Stage {
     /*
     * This class defines a monument, interesting piece of architecture to see
     */
-    constructor() {
-        super();
+    constructor(name, longitude, latitude) {
+        super(name, longitude, latitude);
     }
 }
 
-/* Cette fonction renvoie une série de liste, chaque liste contient, par exemple, tous les noms de tous les évenements. 
-Pour travailler avec un évenement particulier, il suffit de mettre le même index dans chaque liste. Les champs repris de sont, pour l'instant jamais null donc ça ne pose pas de soucis 
-*/
-function getEvent(){
-    fetch('https://data.namur.be/api/records/1.0/search/?dataset=namur-agenda-des-evenements&q=&rows=10&sort=datedebut&facet=title&facet=datedebut&facet=datefin&facet=post_code&facet=acom_nom_m&facet=rue_nom&facet=adr_nopol')
-    .then(res => {return res.json()})
-    .then(data => {
-        const event_name = [];
-        for (let i = 0; i < data.records.length; i++) {
-            if (data.records[i].fields.title != null) {
-            event_name.push(data.records[i].fields.title);} 
-            else {event_name.push("null");}
-        }
-        const event_debut = [];
-        for (let i = 0; i < data.records.length; i++) {
-            if (data.records[i].fields.datedebut != null) {
-            event_debut.push(data.records[i].fields.datedebut);}
-            else {event_debut.push("null");}
-        }
-        const event_fin = [];
-        for (let i = 0; i < data.records.length; i++) {
-            if (data.records[i].fields.datefin != null) {
-            event_fin.push(data.records[i].fields.datefin);}
-            else {event_fin.push("null");}
-        }
-        const event_adress = [];
-        for (let i = 0; i < data.records.length; i++) {
-            if (data.records[i].fields.acom_nom_m != null) {
-            event_adress.push(data.records[i].fields.rue_nom + " " + data.records[i].fields.adr_nopol + ", " + data.records[i].fields.post_code + " " + data.records[i].fields.acom_nom_m);}
-            else {event_adress.push("null");}
-        }
-        const event_description = [];
-        for (let i = 0; i < data.records.length; i++) {
-            if (data.records[i].fields.details != null) {
-            event_description.push(data.records[i].fields.details);}
-            else {event_description.push("null");}
-        }
-        const link = [];
-        for (let i = 0; i < data.records.length; i++) {
-            if (data.records[i].fields.link != null) {
-            link.push(data.records[i].fields.link);}
-            else {link.push("null");}
-        }
-        const coord = [];
-        for (let i = 0; i < data.records.length; i++) {
-            if (data.records[i].fields.geo_point_2d != null) {
-            coord.push(data.records[i].fields.geo_point_2d);}
-            else {coord.push("null");}
-        }
-        document.getElementById('event').innerHTML = event_name + " || " + event_debut + " || " + event_fin + " || " + event_adress + " || " + event_description + " || " + link + ' || ' + coord;
-    })
-    .catch(err => console.log(err));
-    return document.getElementById('event').innerHTML
-}
-function getMuseum() {
-    fetch('https://data.namur.be/api/records/1.0/search/?dataset=namur-annuaire-musees&q=&facet=ville')
-    .then(res => {return res.json()})
-    .then(data2 => {
-        
-        const museum_name = [];
-        for (let i = 0; i < data2.records.length; i++) {
-            if (data2.records[i].fields.titre != null) {
-                museum_name.push(data2.records[i].fields.titre);}
-            else { museum_name.push("null");}
-        }
-        const museum_adress = [];
-        for (let i = 0; i < data2.records.length; i++) {
-            if (data2.records[i].fields.ville != null) {
-            museum_adress.push(data2.records[i].fields.rue + " " + data2.records[i].fields.numero + ", " + data2.records[i].fields.code_postal + " " + data2.records[i].fields.ville);}
-            else { museum_adress.push("null");}
-        }
-        const museum_phone = [];
-        for (let i = 0; i < data2.records.length; i++) {
-            if (data2.records[i].fields.telephone != null) {
-                museum_phone.push(data2.records[i].fields.telephone);}
-            else {museum_phone.push("null");}}
-        const museum_email = [];
-        for (let i = 0; i < data2.records.length; i++) {
-            if (data2.records[i].fields.courriel != null) {
-            museum_email.push(data2.records[i].fields.courriel);}
-            else {museum_email.push("null");}
-        }
-        const museum_description = [];
-        for (let i = 0; i < data2.records.length; i++) {
-            if (data2.records[i].fields.description != null) {
-            museum_description.push(data2.records[i].fields.description);}
-            else {museum_description.push("null");}
-        }
-        const museun_coord = [];
-        for (let i = 0; i < data2.records.length; i++) {
-            if (data2.records[i].fields.geo_point_2d != null) {
-            museun_coord.push(data2.records[i].fields.geo_point_2d);}
-            else {museun_coord.push("null");}
-        }
-        document.getElementById('museum').innerHTML = museum_name + " || " + museum_adress + " || " + museum_phone + " || " + museum_email + " || " + museum_description + " || " + museun_coord;
-        }) 
-        
-     
-    .catch(err => console.log(err));
+async function getEvents(){
+    /*
+    * Cette fonction renvoie une liste d'Event contenant les infos de chaque event
+    */
+    let events = []
 
-    return document.getElementById('museum').innerHTML
+    // Fetch data into JSON
+    let res = await fetch('https://data.namur.be/api/records/1.0/search/?dataset=namur-agenda-des-evenements&q=&rows=999&sort=datedebut&facet=title&facet=datedebut&facet=datefin&facet=post_code&facet=acom_nom_m&facet=rue_nom&facet=adr_nopol');
+    let data = await res.json();
+
+    // Load data into Event instances
+    for (record of data.records) {
+        try {
+            events.push( new Event(
+                record.fields.title != null ? record.fields.title : "null",  // Name
+                record.fields.geo_point_2d[0],  // Longitude
+                record.fields.geo_point_2d[1],  // Latitude
+                record.fields.datedebut != null ? record.fields.datedebut : "null",  // Start
+                record.fields.datefin != null ? record.fields.datefin : "null", // Finish
+                record.fields.acom_nom_m != null ? record.fields.rue_nom + " " + record.fields.adr_nopol + ", " + record.fields.post_code + " " + record.fields.acom_nom_m : "null",  // Address
+                record.fields.details != null ? record.fields.details : "null", // Finish
+                record.fields.link != null ? record.fields.link : "null" // Finish
+            ));
+        }
+        catch {}
+    }
+
+    // Done
+    return events;
 }
 
-// attention que les coordonnées vont par 2 et donc pour chaque monument faut prendre [i] et [i+1]
+async function getMuseums() {
+    /*
+    * Cette fonction renvoie une liste de Museum contenant les infos de chaque musée
+    */
+    let museums = []
 
-function getMonument() {
-    fetch('https://data.namur.be/api/records/1.0/search/?dataset=patrimoine-wallon-monuments&q=&facet=referentie&facet=province&facet=arrondissement&facet=canton&facet=commune')
-    .then(res => {return res.json()})
-    .then(data1 => {
-        const monument_name = [];
-        for (let i = 0; i < data1.records.length; i++) {
-            if (data1.records[i].fields.libelle != null) {
-            monument_name.push(data1.records[i].fields.libelle);}
-            else {monument_name.push("null");}
+    // Fetch data into JSON
+    let res = await fetch('https://data.namur.be/api/records/1.0/search/?dataset=namur-annuaire-musees&q=&facet=ville&rows=999');
+    let data = await res.json();
+
+    // Load data into Museum instances
+    for (record of data.records) {
+        try {
+            museums.push( new Museum(
+                record.fields.titre != null ? record.fields.titre : "null",  // Name
+                record.fields.geo_point_2d != null ? record.fields.geo_point_2d[0] : "null",  // Longitude
+                record.fields.geo_point_2d != null ? record.fields.geo_point_2d[1] : "null",  // Latitude
+                record.fields.ville != null ? record.fields.rue + " " + record.fields.numero + ", " + record.fields.code_postal + " " + record.fields.ville : "null",  // Address
+                record.fields.description != null ? record.fields.description : "null", // Description
+                record.fields.telephone != null ? record.fields.telephone : "null", // Phone
+                record.fields.courriel != null ? record.fields.courriel : "null" // Mail
+            ));
         }
-        const monument_coord = [];
-        for (let i = 0; i < data1.records.length; i++) {
-            if (data1.records[i].fields.geo_point_2d != null) {
-            monument_coord.push(data1.records[i].fields.geo_point_2d)}
-            else {monument_coord.push("null");}
+        catch {}
+    }
+
+    // Done
+    return museums;
+}
+
+async function getMonuments() {
+    /*
+    * Cette fonction renvoie une liste de Monument contenant les infos de chaque monument
+    */
+    let monuments = []
+
+    // Fetch data into JSON
+    let res = await fetch('https://data.namur.be/api/records/1.0/search/?dataset=patrimoine-wallon-monuments&q=&facet=referentie&facet=province&facet=arrondissement&facet=canton&facet=commune&rows=999');
+    let data = await res.json();
+
+    // Load data into Museum instances
+    for (record of data.records) {
+        try {
+            monuments.push( new Monument(
+                record.fields.libelle != null ? record.fields.libelle : "null",  // Name
+                record.fields.geo_point_2d != null ? record.fields.geo_point_2d[0] : "null",  // Longitude
+                record.fields.geo_point_2d != null ? record.fields.geo_point_2d[1] : "null",  // Latitude
+            ));
         }
-        document.getElementById('monument').innerHTML = monument_name + " || " + monument_coord;
-        })
-    .catch(err => console.log(err));
-    return document.getElementById('monument').innerHTML
+        catch {}
+    }
+
+    // Done
+    return monuments;
 }
 
 
-/* 
-
+/*
 <html><body>
   <div id="mapdiv"></div>
   <script src="http://www.openlayers.org/api/OpenLayers.js"></script>
