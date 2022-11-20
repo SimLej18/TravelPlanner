08164f94fceb8384cf140eb73768916b7f93c2f6
SimLej18 <simonlejoly@icloud.com>
2022-11-20 12:20:36 +0100

    Spec canvas for data.js and random_logic.js

--- scripts/data.js
+++ scripts/data.js
@@ -13,6 +13,44 @@ function getName() {
   return name;
 }
 
+class Stage {
+    /*
+    * This class defines a stage (aka. a stop, a halt) in an excursion
+    */
+    constructor() {
+
+    }
+
+}
+
+class Event extends Stage {
+    /*
+    * This class defines an event, stages only available for a limited time
+    */
+    constructor() {
+        super();
+    }
+}
+
+class Museum extends Stage {
+    /*
+    * This class defines a museum, building where you can make a visit
+    */
+    constructor() {
+        super();
+
+    }
+}
+
+class Monument extends Stage {
+    /*
+    * This class defines a monument, interesting piece of architecture to see
+    */
+    constructor() {
+        super();
+    }
+}
+
 /* Cette fonction renvoie une série de liste, chaque liste contient, par exemple, tous les noms de tous les évenements. 
 Pour travailler avec un évenement particulier, il suffit de mettre le même index dans chaque liste. Les champs repris de sont, pour l'instant jamais null donc ça ne pose pas de soucis 
 */
@@ -22,30 +60,51 @@ function getEvent(){
     .then(data => {
         const event_name = [];
         for (let i = 0; i < data.records.length; i++) {
+            if (data.records[i].fields.title != null) {
             event_name.push(data.records[i].fields.title);} 
+            else {event_name.push("null");}
+        }
         const event_debut = [];
         for (let i = 0; i < data.records.length; i++) {
+            if (data.records[i].fields.datedebut != null) {
             event_debut.push(data.records[i].fields.datedebut);}
+            else {event_debut.push("null");}
+        }
         const event_fin = [];
         for (let i = 0; i < data.records.length; i++) {
+            if (data.records[i].fields.datefin != null) {
             event_fin.push(data.records[i].fields.datefin);}
+            else {event_fin.push("null");}
+        }
         const event_adress = [];
         for (let i = 0; i < data.records.length; i++) {
+            if (data.records[i].fields.acom_nom_m != null) {
             event_adress.push(data.records[i].fields.rue_nom + " " + data.records[i].fields.adr_nopol + ", " + data.records[i].fields.post_code + " " + data.records[i].fields.acom_nom_m);}
+            else {event_adress.push("null");}
+        }
         const event_description = [];
         for (let i = 0; i < data.records.length; i++) {
+            if (data.records[i].fields.details != null) {
             event_description.push(data.records[i].fields.details);}
+            else {event_description.push("null");}
+        }
         const link = [];
         for (let i = 0; i < data.records.length; i++) {
+            if (data.records[i].fields.link != null) {
             link.push(data.records[i].fields.link);}
-        document.getElementById('event').innerHTML =  event_name + " || " + event_debut + " || " + event_fin + " || " + event_adress + " || " + event_description + " || " + link
+            else {link.push("null");}
+        }
+        const coord = [];
+        for (let i = 0; i < data.records.length; i++) {
+            if (data.records[i].fields.geo_point_2d != null) {
+            coord.push(data.records[i].fields.geo_point_2d);}
+            else {coord.push("null");}
+        }
+        document.getElementById('event').innerHTML = event_name + " || " + event_debut + " || " + event_fin + " || " + event_adress + " || " + event_description + " || " + link + ' || ' + coord;
     })
     .catch(err => console.log(err));
     return document.getElementById('event').innerHTML
 }
-
-
-// Même chose ici, attention qu'il y a des null, j'ai pas encore traité le problème
 function getMuseum() {
     fetch('https://data.namur.be/api/records/1.0/search/?dataset=namur-annuaire-musees&q=&facet=ville')
     .then(res => {return res.json()})
@@ -53,21 +112,41 @@ function getMuseum() {
         
         const museum_name = [];
         for (let i = 0; i < data2.records.length; i++) {
-            museum_name.push(data2.records[i].fields.titre);}
+            if (data2.records[i].fields.titre != null) {
+                museum_name.push(data2.records[i].fields.titre);}
+            else { museum_name.push("null");}
+        }
         const museum_adress = [];
         for (let i = 0; i < data2.records.length; i++) {
+            if (data2.records[i].fields.ville != null) {
             museum_adress.push(data2.records[i].fields.rue + " " + data2.records[i].fields.numero + ", " + data2.records[i].fields.code_postal + " " + data2.records[i].fields.ville);}
+            else { museum_adress.push("null");}
+        }
         const museum_phone = [];
         for (let i = 0; i < data2.records.length; i++) {
-            museum_phone.push(data2.records[i].fields.telephone);}
+            if (data2.records[i].fields.telephone != null) {
+                museum_phone.push(data2.records[i].fields.telephone);}
+            else {museum_phone.push("null");}}
         const museum_email = [];
         for (let i = 0; i < data2.records.length; i++) {
+            if (data2.records[i].fields.courriel != null) {
             museum_email.push(data2.records[i].fields.courriel);}
+            else {museum_email.push("null");}
+        }
         const museum_description = [];
         for (let i = 0; i < data2.records.length; i++) {
+            if (data2.records[i].fields.description != null) {
             museum_description.push(data2.records[i].fields.description);}
-        document.getElementById('museum').innerHTML = museum_name + " || " + museum_adress + " || " + museum_phone + " || " + museum_email + " || " + museum_description;
-        })
+            else {museum_description.push("null");}
+        }
+        const museun_coord = [];
+        for (let i = 0; i < data2.records.length; i++) {
+            if (data2.records[i].fields.geo_point_2d != null) {
+            museun_coord.push(data2.records[i].fields.geo_point_2d);}
+            else {museun_coord.push("null");}
+        }
+        document.getElementById('museum').innerHTML = museum_name + " || " + museum_adress + " || " + museum_phone + " || " + museum_email + " || " + museum_description + " || " + museun_coord;
+        }) 
         
      
     .catch(err => console.log(err));
@@ -75,7 +154,7 @@ function getMuseum() {
     return document.getElementById('museum').innerHTML
 }
 
-// Ici aucun null donc pas de problème, mais attention que les coordonnées vont par 2 et donc pour chaque monument faut prendre [i] et [i+1]
+// attention que les coordonnées vont par 2 et donc pour chaque monument faut prendre [i] et [i+1]
 
 function getMonument() {
     fetch('https://data.namur.be/api/records/1.0/search/?dataset=patrimoine-wallon-monuments&q=&facet=referentie&facet=province&facet=arrondissement&facet=canton&facet=commune')
@@ -83,13 +162,70 @@ function getMonument() {
     .then(data1 => {
         const monument_name = [];
         for (let i = 0; i < data1.records.length; i++) {
+            if (data1.records[i].fields.libelle != null) {
             monument_name.push(data1.records[i].fields.libelle);}
-        const monument_adress = [];
+            else {monument_name.push("null");}
+        }
+        const monument_coord = [];
         for (let i = 0; i < data1.records.length; i++) {
-            monument_adress.push(data1.records[i].fields.geo_point_2d)}
-
-        document.getElementById('monument').innerHTML = monument_name + " || " + monument_adress;
+            if (data1.records[i].fields.geo_point_2d != null) {
+            monument_coord.push(data1.records[i].fields.geo_point_2d)}
+            else {monument_coord.push("null");}
+        }
+        document.getElementById('monument').innerHTML = monument_name + " || " + monument_coord;
         })
     .catch(err => console.log(err));
     return document.getElementById('monument').innerHTML
 }
+
+
+/* 
+
+<html><body>
+  <div id="mapdiv"></div>
+  <script src="http://www.openlayers.org/api/OpenLayers.js"></script>
+  <style type="text/css">
+      html, body, #basicMap {
+          width: 100%;
+          height: 100%;
+          margin: 0;
+      }
+  </style>
+  <script>
+    map = new OpenLayers.Map("mapdiv");
+    map.addLayer(new OpenLayers.Layer.OSM());
+    
+    var lonLat = new OpenLayers.LonLat(4.86,50.465)
+          .transform(
+            new OpenLayers.Projection("EPSG:4326"), 
+            map.getProjectionObject()
+          );
+          
+    var zoom=15;
+
+    var markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers);
+    
+
+    //Add marker 
+    var event1 = new OpenLayers.LonLat(4.8712975446,50.4633886)
+          .transform(
+            new OpenLayers.Projection("EPSG:4326"), 
+            map.getProjectionObject()
+          );
+
+    var event2 = new OpenLayers.LonLat(4.89,50.47)
+    .transform(
+      new OpenLayers.Projection("EPSG:4326"), 
+      map.getProjectionObject()
+    );
+          
+    markers.addMarker(new OpenLayers.Marker(event1)); //add marker to map
+
+    markers.addMarker(new OpenLayers.Marker(event2));
+    
+    map.setCenter (lonLat, zoom);
+  </script>
+</body></html>
+
+*/
--- scripts/random_logic.js
+++ scripts/random_logic.js
@@ -1,3 +1,56 @@
 /*
-Here we code the random itinary generation.
+Here we code the random itinerary generation.
 */
+
+function generateExcursion() {
+  /*
+  * Generate an excursion from all events recoverable from data.js
+  */
+  let excursion = undefined;
+  let graph = createGraph(3);
+  let valid = false;
+
+  while (!valid) {
+    excursion = getRandomPath(4);
+    valid = validatePath(excursion);
+  }
+
+  return excursion;
+}
+
+function createGraph(linkDistance) {
+  /*
+  * Return a graph with all events recovered from data.js
+  * Events are linked if their distance apart is shorter than linkDistance
+  */
+  let cy = cytoscape({});
+
+  // Add nodes
+
+  // Add links
+
+  // Done
+  return cy;
+}
+
+function getRandomPath(nodeNb) {
+  /*
+  * Generate a random path including <nodeNb> nodes
+  */
+}
+
+function validatePath(path) {
+  /*
+  * Validate a given path. Verifications include :
+  *   -
+  * Return true if the path is valid, false otherwise
+  */
+}
+
+class Utils {
+  /*
+  * This class contains utility methods to simplify the comprehension of the code above.
+  */
+
+
+}
